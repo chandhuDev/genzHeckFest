9a5d8c1d185a209f0b1fb9f013ad2b8c4f51e736
chandhu <chandhu.mpc19@gmail.com>
2022-07-03 19:54:27 +0530

    final commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const app=require("./main")
 require("dotenv").config()
 require("./database/connect").connect()
 
-app.listen(6060,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server listening succesfully")
 })
